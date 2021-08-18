e98a2c4b782d728ee8137694796750911a548f49
Fernandez Gonzalez <andres.fernandez-gonzalez@capgemini.com>
2021-08-18 08:24:56 +0200

    search movie by title

--- peliculas.js
+++ peliculas.js
@@ -1,4 +1,22 @@
+function searchMovieByTitle(){
 
+    var title = document.getElementById("title").value;
+
+    fetch("http://www.omdbapi.com/?t=" + title + "&apikey=55e45ea5")
+    .then(success => success.json()).
+    then((movies) => showMovie(movies) )
+    .catch((error)=>console.log(error));
+
+    document.getElementById("title").value = "";
+   
+
+}
+
+function showMovie(movie){
+
+    document.getElementById("peliculas").innerHTML = '<img  src=' + movie.Poster + 'alt= "movie" />' + 
+    '<div>' +  movie.Title +  '</div>';
+}
 
 
 
