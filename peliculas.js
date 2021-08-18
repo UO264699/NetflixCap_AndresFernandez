d9344cf0935f8cc8954261512eca6488f654cb31
Fernandez Gonzalez <andres.fernandez-gonzalez@capgemini.com>
2021-08-18 09:34:02 +0200

    show movies list

--- peliculas.js
+++ peliculas.js
@@ -19,10 +19,28 @@ function listMovies(){
 
     fetch("http://www.omdbapi.com/?s=" + title + "&apikey=55e45ea5")
     .then(success => success.json()).
-    then((movies) => console.log(movies) )
+    then((movies) => showMovieList(movies) )
     .catch((error)=>console.log(error));
 
     document.getElementById("title").value = "";
+}
+
+
+function showMovieList(movies){
+
+    for(movie of movies.Search){
+
+
+        let div = document.createElement("div")
+
+        document.getElementById("peliculas").appendChild(div)
+
+        div.innerHTML = '<img  src=' + movie.Poster + 'alt= "movie" />' + 
+        '<div> <br>' +  movie.Title + ", " + movie.Year + ' (' + movie.Director +  ')' + '</br></div>' + movie.Plot + "<div>"
+
+
+    }
+
 }
 
 function showMovie(movie){
